be78344bde03d1677fbbd472017cf56b0dc73cb9
antoniom28 <antomarc@gmail.com>
2022-02-15 13:42:17 +0100

    snack B, controllo base, da migliorare

--- snackB.php
+++ snackB.php
@@ -1,6 +1,33 @@
 <?php
 
+$user = [
+    "name" => $_GET[name],
+    "mail" => $_GET[mail],
+    "age" => $_GET[age],
+];
 
+$result = inputControl($user); 
+
+function inputControl($user){
+    if(strlen($user[name]) > 3)
+        if (str_contains($user[mail] ,'@'))
+            if(str_contains($user[mail] ,'.'))
+                return "login successful, hi ".$user[name]."!!";
+            else
+                return "access denied, need .esample for a Mail";
+        else
+            return "access denied, need @ for a Mail";
+    else
+        return "access denied, the name requires 4 or more characters";
+}
+
+/*ho letto che str_contains è per la versione 8 di php, e che 
+la si può implementare in questo modo.
+si poteva fare anche con strpos(..)!= false ma lo avevamo visto
+a lezione quindi ho preferito fare qualcosa che non conoscevo*/
+function str_contains(string $haystack, string $needle): bool{
+    return '' === $needle || false !== strpos($haystack, $needle);
+}
 ?>
 
 <!DOCTYPE html>
@@ -12,6 +39,6 @@
     <title>Document</title>
 </head>
 <body>
-    ciao1
+    <h1> <?php echo $result ?> </h1>
 </body>
 </html>
