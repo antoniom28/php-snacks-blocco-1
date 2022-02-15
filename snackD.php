9225c83af4cdc5c35e1c9647e2e1126fc6578bb2
antoniom28 <antomarc@gmail.com>
2022-02-15 14:54:24 +0100

    snack D

--- snackD.php
+++ snackD.php
@@ -6,8 +6,13 @@ tanti paragrafi. Ogni punto un nuovo paragrafo.*/
 $text = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam doloremque porro asperiores est ipsam nobis modi magnam aliquid, eligendi, nostrum animi consectetur. Debitis provident et, vitae culpa praesentium sint maiores?
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid illo dolore laboriosam quasi qui iste cum tempore unde omnis quos, ducimus necessitatibus? Mollitia quo, nihil harum illo nisi fugit? Cumque.";
 
-$sub_text = explode(".",$text);
+//se finisse senza un punto, eliminerebbe un paragrafo
+//inoltre toglie il delimiter dai paragrafi
+//$sub_text = explode(".",$text,-1);
 
+// /(?= delimiter)/ metterà delimiter all'inizio dell'array successivo
+// /(?<= delimiter)/ è un explode che però mantiene il delimiter
+$sub_text = preg_split('/(?<=\.)/', $text);
 
 ?>
 
@@ -20,12 +25,9 @@ $sub_text = explode(".",$text);
     <title>Document</title>
 </head>
 <body>
-    <?php
-
+    <?php 
+        for($i=0; $i<count($sub_text); $i++) 
+            echo '<p>' . $sub_text[$i] .'</p>'; 
     ?>
 </body>
 </html>
-
-<?php
-var_dump($sub_text);
-?>
